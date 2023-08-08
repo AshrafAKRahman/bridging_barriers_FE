2f40457cff9e1fca07fc7173a6503dbbd07e5a56
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-08-08 14:31:10 +0100

    admin restriction applied

--- app/page.js
+++ app/page.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <p>Hello World !!!</p>
+        <p>Welcome back !!!</p>
+      </div>
+    </main>
   );
 }
