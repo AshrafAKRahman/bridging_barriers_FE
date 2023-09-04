4acac4b70058ea321776c195c19a4df9e44abb41
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-09-04 10:17:39 +0100

    animation added to profile page

--- app/components/subHeader/subHeader.jsx
+++ app/components/subHeader/subHeader.jsx
@@ -1,8 +1,18 @@
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: "900",
+  preload: true,
+});
+
 const SubHeader = ({ title, titleClassName }) => {
   return (
-    <header className="text-center">
-      <h2 className={`${titleClassName} font-bold`}>{title}</h2>
-    </header>
+    <div className={raleway.className}>
+      <header className="text-center">
+        <h2 className={`${titleClassName} font-bold`}>{title}</h2>
+      </header>
+    </div>
   );
 };
 
--- app/pages/profile/page.js
+++ app/pages/profile/page.js
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from "react";
 import { UserAuth } from "../../context/AuthContext";
 import Form from "../../components/form/form";
 import Header from "../../components/header/header";
-import NormalButton from "../../components/buttons/normalButton";
 import Link from "next/link";
 import { useFormContext } from "../../context/FormContext";
 import InputFeild from "@/app/components/inputFeild/inputFeild";
 import LargeButton from "@/app/components/buttons/largeButton";
+import { gsap } from "gsap";
 
 const Profile = () => {
   const { user, newUser } = UserAuth();
@@ -16,6 +16,47 @@ const Profile = () => {
   const [loading, setLoading] = useState();
   const [genderInput, setGenderInput] = useState(false);
   const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const reveal = () => {
+      const FADE = gsap.timeline();
+
+      FADE.from(".backdrop", {
+        autoAlpha: 0,
+        y: -100,
+        duration: 1,
+      });
+    };
+    reveal();
+  }, []);
+
+  useEffect(() => {
+    const reveal = () => {
+      const FADE = gsap.timeline();
+
+      FADE.from(".fname, .sname, .dob, .gender", {
+        autoAlpha: 0,
+        x: -50,
+        stagger: 0.25,
+        delay: 0.75,
+      });
+    };
+    reveal();
+  }, []);
+
+  useEffect(() => {
+    const reveal = () => {
+      const FADE = gsap.timeline();
+
+      FADE.from(".phone, .email, .password, .location", {
+        autoAlpha: 0,
+        x: 50,
+        stagger: 0.25,
+        delay: 1.5,
+      });
+    };
+    reveal();
+  }, []);
 
   useEffect(() => {
     if (typeof localStorage !== "undefined") {
@@ -65,15 +106,15 @@ const Profile = () => {
   }, [user]);
 
   return (
-    <div className="h-screen md:w-screen bg-blue-500 flex flex-col items-center justify-center">
-      <div className="h-full pt-10 pb-32 w-full flex flex-col items-center md:h-full md:pb-20">
+    <div className="backdrop h-screen md:w-screen bg-blue-500 flex flex-col items-center justify-center invisible">
+      <div className=" h-full pt-10 pb-32 w-full flex flex-col items-center md:h-full md:pb-20">
         <Form>
-          <div className="h-full w-full md:flex md:justify-between ">
+          <div className="  h-full w-full md:flex md:justify-between ">
             <div className="w-full h-full flex justify-center items-center">
               <div className="bg-black opacity-40 absolute h-[70%] w-4/5 rounded-2xl md:h-[70%] md:w-[70%] md:mb-10 z-10"></div>
-              <div className="w-[80%] h-4/6 absolute md:w-[60%] md:h-5/6 ">
+              <div className="w-[80%] h-4/6 absolute md:w-[60%] md:h-5/6">
                 <img
-                  className="img rounded-2xl object-cover h-full w-full "
+                  className=" rounded-2xl object-cover h-full w-full "
                   src="/profileImg.jpg"
                   alt="form igmage"
                 />
@@ -81,8 +122,8 @@ const Profile = () => {
 
               <div className="h-full w-full md:flex md:justify-between md:items-center md:mb-10 z-20 ">
                 <div className=" h-1/2 w-full flex flex-col items-center justify-evenly md:w-1/2 md:h-5/6">
-                  <div className="w-5/6 flex justify-between">
-                    <div className="text-white md:ml-16">
+                  <div className="fname w-5/6 flex justify-between invisible">
+                    <div className=" text-white md:ml-16">
                       <label>
                         <Header
                           title="First name"
@@ -100,7 +141,7 @@ const Profile = () => {
                       />
                     </div>
                   </div>
-                  <div className="w-5/6 flex justify-between">
+                  <div className="sname w-5/6 flex justify-between invisible">
                     <div className="text-white md:ml-16 ">
                       <label>
                         <Header
@@ -119,7 +160,7 @@ const Profile = () => {
                       />
                     </div>
                   </div>
-                  <div className="w-5/6 flex justify-between">
+                  <div className="dob w-5/6 flex justify-between invisible">
                     <div className="text-white md:ml-16 ">
                       <label>
                         <Header
@@ -137,7 +178,7 @@ const Profile = () => {
                       />
                     </div>
                   </div>
-                  <div className="w-5/6 flex justify-between">
+                  <div className="gender w-5/6 flex justify-between invisible">
                     <div className="text-white md:ml-16 ">
                       <label>
                         <Header
@@ -177,7 +218,7 @@ const Profile = () => {
                   </div>
                 </div>
                 <div className="h-1/2 w-full flex flex-col items-center justify-evenly md:w-1/2 md:h-5/6 md:mr-8">
-                  <div className="w-5/6 flex justify-between ">
+                  <div className="phone w-5/6 flex justify-between invisible">
                     <div className="text-white md:ml-12">
                       <label>
                         <Header
@@ -196,7 +237,7 @@ const Profile = () => {
                       />
                     </div>
                   </div>
-                  <div className="w-5/6 flex justify-between  ">
+                  <div className="email w-5/6 flex justify-between invisible">
                     <div className="text-white md:ml-12">
                       <label>
                         <Header
@@ -215,7 +256,7 @@ const Profile = () => {
                       />
                     </div>
                   </div>
-                  <div className="w-5/6 flex justify-between">
+                  <div className="password w-5/6 flex justify-between invisible">
                     <div className="text-white md:ml-12">
                       <label>
                         <Header
@@ -234,7 +275,7 @@ const Profile = () => {
                       />
                     </div>
                   </div>
-                  <div className="w-5/6 flex justify-between">
+                  <div className="location w-5/6 flex justify-between invisible">
                     <div className="text-white md:ml-16">
                       <label>
                         <Header
