f9e545712202492d001e308b70ed4281e42cba8c
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-10-03 19:10:26 +0100

    debug

--- app/components/navbar/navBar.jsx
+++ app/components/navbar/navBar.jsx
@@ -1,20 +1,40 @@
-import NormalButton from "@/app/components/normalButton/normalButton";
-import Image from "next/image";
+"use client";
+
+import { gsap } from "gsap";
+import NormalButton from "../buttons/normalButton";
+import Image from "next/legacy/image";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import LargeButton from "../buttons/largeButton";
 
 const Navbar = () => {
+  useEffect(() => {
+    const revealAnim = () => {
+      const TLFADE = gsap.timeline();
+
+      TLFADE.from(".nav", {
+        autoAlpha: 0,
+        y: -50,
+        duration: 1,
+      });
+    };
+    revealAnim();
+  }, []);
+
   return (
-    <nav className="flex items-center justify-between w-full p-6 bg-white">
-      <div className="text-white text-2xl">
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          className="mr-2"
-          height={22}
-          width={200}
-        />
-      </div>
-      <div className="mr-0">
-        <NormalButton text="Login" />
+    <nav className="w-screen">
+      <div className="nav fixed flex items-center w-full h-20  bg-white p-8 border-solid border-b-4 border-blue-500 invisible ">
+        <div className="w-screen flex items-center justify-center md:flex md:justify-between md:items-cente">
+          <div className="w-48  h-fit">
+            <img src="/logo.png" alt="Logo" className="" />
+          </div>
+          <div className=" mt-0 hidden md:flex">
+            <Link href="/pages/login">
+              {/* <p className=" text-blue-600">LOGIN</p> */}
+              <LargeButton text="LOGIN" />
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
