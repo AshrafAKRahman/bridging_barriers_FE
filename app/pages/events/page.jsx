70504b857ef331dc87d270a0e3942226a0cf1c91
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-11-28 15:38:31 +0100

    trying to establish the connection to eventbrite api

--- app/pages/events/page.jsx
+++ app/pages/events/page.jsx
@@ -3,3 +3,36 @@ const Events = () => {
 };
 
 export default Events;
+
+// "use client";
+
+// import { useEffect, useState } from "react";
+
+// const Events = () => {
+//   const [events, setEvents] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch("../api/events");
+//         const data = await response.json();
+//         setEvents(data);
+//         console.log("Your events:", data);
+//       } catch (error) {
+//         console.error("Error fetching events:", error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       {events.map((event) => (
+//         <div key={event.id}>{event.name}</div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Events;
