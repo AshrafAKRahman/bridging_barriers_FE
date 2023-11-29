77960534b045fc174ef9d5694862dbf127324c44
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-11-29 11:51:24 +0100

    added the server filefor the eventbrite API

--- app/server.js
+++ app/server.js
@@ -1,11 +1,11 @@
-// import express from "express";
-// import fetch from "node-fetch";
+import express from "express";
+import fetch from "node-fetch";
 
-// const app = express();
-// const PORT = process.env.PORT;
+const app = express();
+const PORT = process.env.PORT;
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.get("/api/events:eventID", async (req, res) => {
-//   const eventId = "21750539";
-// });
+app.get("/api/events:eventID", async (req, res) => {
+  const eventId = "21750539";
+});
