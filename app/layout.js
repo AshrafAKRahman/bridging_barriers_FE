2e678bfcb4c81a344ccfb7aed63e0677ef37fe4b
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-11-30 13:05:59 +0100

    removed parallax provider package

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,6 @@
 import Head from "next/head";
 import { AuthContextProvider } from "./context/AuthContext";
 import { FormProvider } from "./context/FormContext";
-import { ParallaxProvider } from "react-scroll-parallax";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -17,33 +16,9 @@ export default function RootLayout({ children }) {
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
         />
       </Head>
-      <ParallaxProvider scrollAxis="vertical">
-        <AuthContextProvider>
-          <FormProvider>{children}</FormProvider>
-        </AuthContextProvider>
-      </ParallaxProvider>
+      <AuthContextProvider>
+        <FormProvider>{children}</FormProvider>
+      </AuthContextProvider>
     </>
   );
 }
-
-// import { AuthContextProvider } from "./context/AuthContext";
-// import "./globals.css";
-// import { FormProvider } from "./context/FormContext";
-// import { ParallaxProvider } from "react-scroll-parallax";
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <ParallaxProvider scrollAxis="vertical">
-//         <body>
-//           <AuthContextProvider>
-//             <FormProvider>
-//               {children}
-//               {/* <Footer /> */}
-//             </FormProvider>
-//           </AuthContextProvider>
-//         </body>
-//       </ParallaxProvider>
-//     </html>
-//   );
-// }
