6cd546e958bbf4608ac97b49832b68f09041a078
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-12-12 13:38:32 +0100

    restyled the dashboard

--- app/pages/loggedIn/page.jsx
+++ app/pages/loggedIn/page.jsx
@@ -30,8 +30,8 @@ const LogedIn = () => {
   return (
     <div className="w-screen h-screen bg-blue-500 pb-10">
       <Navbar />
-      <div className="w-full h-full flex flex-col justify-center items-center md:flex-row md:justify-center md:items-center ">
-        <div className="flex w-full h-1/2 flex-col items-center justify-center md:mt-10 md:justify-center md:items-center md:w-1/2 md:h-full">
+      <div className="w-full h-full flex flex-col justify-center items-center md:flex-row md:justify-center md:items-center">
+        <div className="flex w-full h-full flex-col items-center justify-center md:mt-10 md:justify-center md:items-center md:w-1/2 md:h-full">
           <div className="bg bg-gray-300 absolute w-5/6 h-2/6 mt-20  flex flex-col items-center justify-center bg-opacity-40 backdrop-blur-md rounded-2xl md:h-2/3 md:w-1/4 md:mt-0">
             <div className="w-full h-1/2 flex justify-center items-center">
               <CgProfile size={104} />
@@ -46,15 +46,15 @@ const LogedIn = () => {
         </div>
 
         <div className="h-1/2 w-full flex justify-center items-center pb-10 md:pb-0 md:mt-10 md:mr-10 md:w-1/2 md:h-full">
-          <div className="bg bg-gray-300 absolute w-5/6 h-1/3 flex flex-col items-center justify-center bg-opacity-40 backdrop-blur-md rounded-2xl md:h-3/4 md:w-3/6 md:mt-10 md:mr-16">
-            <div className="w-fit h-1/4 flex justify-center pt-5 md:pt-16">
+          <div className="bg bg-gray-300 absolute w-5/6 h-2/3 flex flex-col items-center justify-center bg-opacity-40 backdrop-blur-md rounded-2xl md:h-3/4 md:w-3/6 md:mt-10 md:mr-16">
+            <div className="w-fit h-fit flex justify-center pt-5 md:pt-16">
               <Header
                 titleClassName="text-2xl text-gray-700  text-center md:text-4xl"
                 title="Your Saved Items"
               />
             </div>
-            <div className="h-4/5 w-5/6 flex flex-col items-center justify-evenly mb-5 ">
-              <div className="w-5/6 h-1/4 bg-white flex items-center justify-center border-solid border-2 border-sky-500 rounded-lg my-2 md:w-5/6 md:h-1/4 ">
+            <div className="h-full w-full flex flex-col items-center justify-evenly mb-5 ">
+              <div className="w-full h-2/3 bg-white flex items-center justify-center border-solid border-2 border-sky-500 rounded-lg my-2 md:w-5/6 md:h-1/4 ">
                 <img
                   src="/blog.png"
                   alt="blog image"
@@ -68,7 +68,7 @@ const LogedIn = () => {
                 </div>
               </div>
 
-              <div className="w-5/6 h-1/4 bg-white flex items-center justify-evenly border-solid border-2 border-sky-500 rounded-lg my-2">
+              <div className="w-full h-2/3 bg-white flex items-center justify-evenly border-solid border-2 border-sky-500 rounded-lg my-2">
                 {savedEvents.map((savedEvent) => (
                   <div key={savedEvent.id}>
                     <p>{savedEvent.name.text}</p>
@@ -83,6 +83,7 @@ const LogedIn = () => {
                   </div>
                 ))}
                 {savedEvents.length === 0 && <p>No saved events</p>}
+                {savedEvents.id}
               </div>
 
               <div className="w-5/6 h-1/4 bg-white flex items-center justify-center border-solid border-2 border-sky-500 rounded-lg my-5">
